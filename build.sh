727d7fb208bd1f44d71b87665a48bcce14449a66
sameh <sameh.java@gmail.com>
2024-08-16 01:18:25 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -2,26 +2,26 @@
 if [[ "$*" =~ -b ]]; then
 ./mvnw verify
 fi
-#echo $REPO_USER
-#echo $REPO_PASS
-#JARS="$(cat JARS.txt)"
-OPENFIRE_PLUGINS="http://161.35.28.94:8081/artifactory/simple/appx-maven-release/co/kartngo"
-ary=($JARS)
 
-jars_file="JARS.txt"
-if [[ ! -f "$jars_file" ]]; then
-    echo "Missing $jars_file !"
-    exit 1
-fi
+if [[ "$*" =~ -U ]]; then
+    jars_file="JARS.txt"
+    jarArray=($(<"$jars_file"))
+  OPENFIRE_PLUGINS="http://161.35.28.94:8081/artifactory/simple/appx-maven-release/co/kartngo"
+  ary=($JARS)
 
-jarArray=($(<"$jars_file"))
-
-for jarPath in "${jarArray[@]}"; do
+  jars_file="JARS.txt"
+  if [[ ! -f "$jars_file" ]]; then
+      echo "Missing $jars_file !"
+      exit 1
+  fi
+  for jarPath in "${jarArray[@]}"; do
     echo "Downloading Jar : $jarPath"
     jarFile="${jarPath%%/*}"
     jarFile="${jarFile}.jar"
     echo $jarFile
     wget --user $REPO_USER --password $REPO_PASS -O distribution/target/distribution-base/plugins/$jarFile "${OPENFIRE_PLUGINS}/$jarPath"
-done
+  done
+fi
 
 
+docker build -t samehkartngo/openfire:dev .
